7752b1218a3376f3f8c763487a9cbebafde5935e
ReBel <mrxhm1555@gmail.com>
2018-12-02 16:29:01 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,171 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('message',async message => {
+    if (message.content.startsWith('#js')) {
+  if(!message.channel.guild) return message.reply('This Command For Servers Only !');
+  let staff = message.guild.member(message.author).roles.find('name' , 'Codes Support');// رتبه السبورت
+  if(!staff) return msg.reply('🛑 You Cant Do This Command')
+      let jscodes = message.guild.channels.find(`name`, "discord-js");// روم يلي ينشر الكود فيه
+    if(!jscodes) return message.channel.send(":x:لم اجد الروم الخاص بنشر الاكواد");
+      let filter = m => m.author.id === message.author.id;
+      let thisMessage;
+      let thisFalse;
+      message.channel.send(':pencil: **| من فضلك اكتب الكود الأن... :pencil2: **').then(msg => {
+  
+      message.channel.awaitMessages(filter, {
+        max: 1,
+        time: 90000,
+        errors: ['time']
+      })
+      .then(collected => {
+        collected.first().delete();
+        thisMessage = collected.first().content;
+        let boi;
+        msg.edit(':scroll: **| من فضلك اكتب وصف الكود الأن... :pencil2: **').then(msg => {
+  
+            message.channel.awaitMessages(filter, {
+              max: 1,
+              time: 90000,
+              errors: ['time']
+            })
+            .then(collected => {
+              collected.first().delete();
+              boi = collected.first().content;
+              let boi2;
+              msg.edit(':man_in_tuxedo: **| من فضلك اكتب من صنع هذا الكود الأن... :pencil2: **').then(msg => {
+  
+                message.channel.awaitMessages(filter, {
+                  max: 1,
+                  time: 90000,
+                  errors: ['time']
+                })
+                .then(collected => {
+                  collected.first().delete();
+                boi2 = collected.first().content;
+        msg.edit(':shield: **| [ هل انت متأكد من نشر الكود؟ | [ نعم ] او [ لا**');
+   message.channel.awaitMessages(response => response.content === 'نعم' || 'لا' && filter,{
+          max: 1,
+          time: 90000,
+          errors: ['time']
+        })
+        .then(collected => {
+          if(collected.first().content === 'لا') {
+            msg.delete();
+            message.delete();
+            thisFalse = false;
+          }
+          if(collected.first().content === 'نعم') {
+            if(thisFalse === false) return;
+            msg.edit(':dove: **| Done :white_check_mark:, تم بنجاح نشر كودك في روم الاكواد**');
+            collected.first().delete();
+            jscodes.send(`@everyone | @here
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+**${message.guild.name}© :arrow_down:**            
+\`\`\`js
+${thisMessage}\`\`\`
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+**وصف الكود**: ${boi}
+**تم النشر بواسطة**: ${message.author}
+**المصدر / الشخص الذي صنع الكود**: ${boi2}`); 
+          }
+        }
+    );
+});
+      });
+    }
+      );
+    });
+}
+);
+      })}});
+      
+
+
+client.on('message', async rokz => {
+ 
+    if(rokz.content.startsWith(prefix + "تقديم")) {
+ 
+      let lang = '';
+ 
+      let time = '';
+ 
+      let expe = '';
+ 
+      let fillter = m => m.author.id === rokz.author.id
+ 
+      await rokz.channel.send("ما لغتك ؟").then(e => {
+ 
+     rokz.channel.awaitMessages(fillter, { time: 60000, max: 1 })
+ 
+     .then(co => {
+ 
+       lang = co.first().content;
+ 
+        co.first().delete();
+ 
+ 
+       e.edit(`كم هي المدة ؟
+[${lang}]`)
+ 
+       rokz.channel.awaitMessages(fillter, { time: 60000, max: 1 })
+ 
+       .then(col => {
+ 
+         time = col.first().content;
+ 
+          col.first().delete();
+ 
+ 
+            e.edit(`ما هي خبرتك ؟
+[${time}]
+[${lang}]`)
+ 
+            rokz.channel.awaitMessages(fillter, { time: 60000, max: 1 })
+ 
+            .then(coll => {
+ 
+              expe = coll.first().content;
+ 
+               coll.first().delete();
+ 
+ 
+               e.edit(`جاري تقديمك...
+[${expe}]
+[${time}]
+[${lang}]`)
+ 
+              let rokzz = rokz.guild.channels.find("name","تقديم")
+ 
+              setTimeout(() => {
+ 
+                e.edit("تم التقديم")
+ 
+              }, 3000)
+ 
+              rokzz.send(`
+» اللغة : **${lang}**
+» المدة : **${time}**
+» الخبرة : **${expe}**
+تم التقديم بواسطة: ${rokz.author}
+`).then(rokzzz => {
+ 
+                  rokzzz.react("✅")
+ 
+                  rokzzz.react("❌")
+ 
+                })
+ 
+            })
+ 
+       })
+ 
+     })
+ 
+   })
+ 
+    }
+ 
+  })
+  
+client.login(process.env.BOT_TOKEN);
